eb0781015c9ddf97b3ad58d8d50c20fb62884f2a
Florian Friesdorf <flo@chaoflow.net>
2013-04-22 23:38:30 +0200

    no reason to "hide" the OrderedDict

--- src/tpv/ordereddict.py
+++ src/tpv/ordereddict.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 
 try:
-    from collections import OrderedDict as _OrderedDict
+    from collections import OrderedDict
 except ImportError:
-    from ordereddict import OrderedDict as _OrderedDict
+    from ordereddict import OrderedDict
 
 from . import aspects
 
 
 @aspects.set_children_on_init
 @aspects.getattr_children
-class Node(_OrderedDict):
+class Node(OrderedDict):
     pass
