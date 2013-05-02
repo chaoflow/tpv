397d2e30d093f4691ff61cd7e7b3760ed0a66231
Alexander Pilz <pilz@syslab.com>
2013-05-02 13:45:10 +0200

    added infrastructure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0'
+version = '0.1.dev0'
 shortdesc = 'Core of the vortex.'
 
 #longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
