66fccf0eb032326258c7f6167af0e274fd5e7200
Florian Friesdorf <flo@chaoflow.net>
2013-07-26 00:18:22 +0200

    children_from_entry_points

---
+++ src/tpv/pkg_resources.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+import pkg_resources
+
+from metachao import aspect
+
+
+class children_from_entry_points(aspect.Aspect):
+    _entry_point_group = aspect.config(entry_point_group=None)
+
+    @aspect.plumb
+    def __init__(_next, self, **kw):
+        _next(**kw)
+        eps = sorted(pkg_resources.iter_entry_points(self._entry_point_group),
+                     key=lambda x: x.name)
+        for ep in eps:
+            path = ep.name.split('/')
+            name = path.pop()
+            parent = self
+            while path:
+                parent = parent[path.pop(0)]
+            parent[name] = ep.load()
